7084d8a2fc7c6eabd326f6120b6aeb36af7b435f
LeoG <guoyou0105@163.com>
2022-05-03 16:38:47 +0800

    依赖注入3：set注入与构造器注入；基本数据类型注入；引用数据类型注入；集合注入

--- iteima_spring_ioc/src/main/java/com/guoyou/service/UserServiceImpl.java
+++ iteima_spring_ioc/src/main/java/com/guoyou/service/UserServiceImpl.java
@@ -8,8 +8,17 @@ public class UserServiceImpl implements UserService {
 
     private UserDao userDao;
 
-    public void setUserDao(UserDao userDao) {
+    //1.set注入，需要在xml文件中注入
+//    public void setUserDao(UserDao userDao) {
+//        this.userDao = userDao;
+//    }
+
+    //2.构造方法注入
+    public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public UserServiceImpl() {
     }
 
     @Override
